b88c8053f7c4811bb0fd2bb57bf503a9d10cab30
progress0407 <progress0407@gmail.com>
2024-01-17 18:46:35 +0900

    implement core of crawling kakao comments

---
+++ src/main/kotlin/io/philo/matzip/app/Crawling.kt
@@ -0,0 +1,71 @@
+package io.philo.matzip.app
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.http.ResponseEntity
+import org.springframework.web.client.RestClient
+import java.lang.Thread.sleep
+
+fun main() {
+    val restClient: RestClient = RestClient.create();
+
+    val url = Url("https://place.map.kakao.com/main/v/1619702502")
+
+    do {
+        var isOkStatus = true
+        println("# 탐색: ${url.value}")
+        val entity: ResponseEntity<Dto> = restClient.get()
+            .uri(url.value)
+            .accept(APPLICATION_JSON)
+            .retrieve()
+            .onStatus(HttpStatusCode::is4xxClientError) { request, response ->
+                isOkStatus = false
+                println("is Not Status OK ${response.statusCode}")
+            }
+            .toEntity(Dto::class.java)
+
+        if (isOkStatus.not() || isEmpty(entity)) {
+            println("# empty ")
+            println("status = ${entity.statusCode}")
+            break
+        }
+
+        val comments = entity.body!!.comment!!.list!!
+        println("comments = ${comments}")
+
+        url.setLastUrl(comments.last().commentid)
+
+        sleep(500)
+
+    } while (entity.statusCode == HttpStatus.OK)
+}
+
+private fun isEmpty(entity: ResponseEntity<Dto>) =
+    entity.body == null || entity.body!!.comment == null || entity.body!!.comment!!.list == null
+
+data class Url(var value: String) {
+
+    var isFirstStatus = true
+
+    fun setLastUrl(lastCommentId: Long) {
+        if (isFirstStatus) {
+            value = value.replace("/main", "/commentlist") + "/$lastCommentId"
+            isFirstStatus = false
+        } else {
+            value = "\\d+$".toRegex().replace(value, lastCommentId.toString())
+        }
+    }
+}
+
+data class Dto(val comment: DtoList?) {
+    constructor() : this(null)
+}
+
+data class DtoList(val list: List<DtoComment>?) {
+    constructor() : this(null)
+}
+
+data class DtoComment(val commentid: Long, val contents: String?) {
+    constructor() : this(-1, null)
+}
