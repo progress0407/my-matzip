fd8c9ce291a91a216c8fecde7d09e0ad6eb78694
progress0407 <progress0407@gmail.com>
2024-01-17 18:45:43 +0900

    init and create project

---
+++ src/main/kotlin/io/philo/matzip/MatzipApplication.kt
@@ -0,0 +1,11 @@
+package io.philo.matzip
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MatzipApplication
+
+fun main(args: Array<String>) {
+	runApplication<MatzipApplication>(*args)
+}
---
+++ src/test/kotlin/io/philo/matzip/MatzipApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.philo.matzip
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MatzipApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
